6ebac3612c5295f6c641f0632017a8d0cb3d8815
elmiller <elmiller@gmail.com>
2013-07-17 13:45:09 -0700

    Added command line arguments
    Script will now take year and month from the command line.

--- calcreate.py
+++ calcreate.py
@@ -1,41 +1,45 @@
 #!/usr/bin/python
 
-#importing modules
 import datetime
 from datetime import timedelta
 import sys
 
-#get user input for month
-entermonth = input("Enter month number: ")
+# Test if arguments were set at the command line
+if len(sys.argv) > 2:
+	enteryear = int(sys.argv[1])
+	entermonth = int(sys.argv[2])
 
-#validate input
-if entermonth in range(1,13):
-	thismonth = entermonth
+# If no arguments, read input with prompt
 else:
-	print "Error: enter 1-12."
-	sys.exit()
+	# Get user input for year
+	enteryear = input("Enter year: ")
+	# Get user input for month
+	entermonth = input("Enter month number: ")
 
-#get user input for year
-enteryear = input("Enter year: ")
-
-#validate input
+# Validate input for year
 if enteryear in range(2012,2021):
 	thisyear = enteryear
 else:
-	print "Error: enter 2012-2020."
+	print "Error: enter a year in the range 2012-2020."
 	sys.exit()
 
+# Validate input
+if entermonth in range(1,13):
+	thismonth = entermonth
+else:
+	print "Error: enter 1-12 for the month."
+	sys.exit()
 
-#set calendar variables
+# Set calendar variables
 now = datetime.datetime.now()
-#thisyear = now.year
+
 startday = 1
 startdate = datetime.date(thisyear,thismonth,startday)
 
-#loop through the month and print date and subjects
+# Loop through the month and print date and subjects
 for x in range(0,31):
 	eachday = startdate + datetime.timedelta(days=x)
-	#include only weekdays
+	# Include only weekdays
 	if eachday.weekday() in range(0,5):
 		print eachday.strftime("%A, %B %d, %Y")
 		print "LA"
