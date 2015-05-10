1116ae6de59c3e267001ecaf12344d4fe8241a58
elmiller <elmiller@gmail.com>
2015-05-09 22:58:03 -0700

    Fixed bugs related to dates
    Fixed bug that printed wrong month in title.
    Fixed date range so it only loops though the days of the month (i.e. 28
    for February) instead of always doing 31 days)
    Improved formatting of single digit dates.

--- mdcal.py
+++ mdcal.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 
-import datetime, calendar, sys, re
+import datetime
 from datetime import timedelta
+import sys
 
 # Create lists for each day
-Monday = ['LA', 'EH']
-Tuesday = ['LA', 'EH', 'HR']
-Wednesday = ['LA', 'EH']
-Thursday = ['LA', 'EH', 'HR']
-Friday = ['LA', 'EH']
+Monday = ['LA\n\n1. Update planners\n2.\n', '\nEH']
+Tuesday = ['LA', '\nEH', '\nHR']
+Wednesday = ['LA', '\nEH']
+Thursday = ['LA', '\nEH', '\nHR']
+Friday = ['LA\n\n1. Reading Log Reflection\n2.\n', '\nEH']
 
 # Create function that loops through the lists and prints the teachers and events for the day.
 def printSchedule(day):
@@ -30,10 +31,10 @@ else:
 	entermonth = input("Enter month number (mm): ")
 
 # Validate input for year
-if enteryear in range(2012,2051):
+if enteryear in range(2012,2021):
 	thisyear = enteryear
 else:
-	print "Error: enter a year in the range 2012-2050."
+	print "Error: enter a year in the range 2012-2020."
 	sys.exit()
 
 # Validate input for month
@@ -46,28 +47,24 @@ else:
 # Set calendar variables
 now = datetime.datetime.now()
 
-startdate = datetime.date(thisyear,thismonth,1)
+startday = 1
+startdate = datetime.date(thisyear,thismonth,startday)
 
-# Find how many days are in the current month.
-daysinmonth = calendar.monthrange(thisyear, thismonth)[1]
+# Print Monday as heading
 
-print startdate.strftime("%B %Y\n")
+print now.strftime("# %B %Y\n")
 
 # Loop through the month and print date and subjects
-for x in range(1,daysinmonth):
+for x in range(0,31):
 	eachday = startdate + datetime.timedelta(days=x)
-
+	
 	# Print "Week of..."
 	if eachday.weekday() in range(1):
-		weekline = eachday.strftime("Week of %A, %B %e, %Y\n")
-		# Remove repeated spaces date is single digit number
-		print re.sub(r"\s\s+", " ",weekline)
+		print eachday.strftime("## Week of %A, %B %d, %Y\n")
 
 	# Include only weekdays
 	if eachday.weekday() in range(0,5):
-		dayline = eachday.strftime("%A, %B %e, %Y")
-		# Remove repeated spaces date is single digit number
-		print re.sub(r"\s\s+", " ",dayline)
+		print eachday.strftime("%A, %B %d, %Y\n")
         # Sets value of theday to a day of the week (i.e. "Monday")
         theday = eachday.strftime("%A")
         if theday == "Monday":
@@ -80,3 +77,4 @@ for x in range(1,daysinmonth):
             printSchedule(Thursday)
         elif theday == "Friday":
             printSchedule(Friday)
+            print "<br><hr><br>"
